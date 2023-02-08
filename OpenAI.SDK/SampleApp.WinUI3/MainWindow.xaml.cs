1cf71d837e434958337e0842c1f4015d55347100
Robson <rp.pontin@gmail.com>
2023-02-07 22:03:56 -0800

    Updated WinUI 3 sample

--- OpenAI.SDK/SampleApp.WinUI3/MainWindow.xaml.cs
+++ OpenAI.SDK/SampleApp.WinUI3/MainWindow.xaml.cs
@@ -2,37 +2,37 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 
 namespace SampleApp.WinUI3
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        OpenAI.OpenAiService m_openAiService;
+
         public MainWindow()
         {
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
-            //var req = new OpenAI.ImageCreateRequest();
+            if (m_openAiService == null)
+            {
+                m_openAiService = new OpenAI.OpenAiService();
+                m_openAiService.SetOpenAiSecretKey("YOU_OPEN_AI_KEY");
+            }
+
+            var imgRequest = new OpenAI.Image.ImageCreateRequest();
+            imgRequest.Prompt = "A robot bird flying above a green lake, abstract, art";
+
+            var result = await m_openAiService.RunRequestAsync(imgRequest);
+            if (result.IsResponseSuccess)
+            {
+                // Handle image received
+                //var img = result.Images.First();
+            }
         }
     }
 }
