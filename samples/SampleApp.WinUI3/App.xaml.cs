52a5a23f1aff3b960784ccbbd834979fd5a3d056
Robson <rp.pontin@gmail.com>
2023-08-12 10:31:25 -0700

    cleanup

--- samples/SampleApp.WinUI3/App.xaml.cs
+++ samples/SampleApp.WinUI3/App.xaml.cs
@@ -3,25 +3,7 @@
 
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using SampleApp.WinUI3.Views;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace SampleApp.WinUI3
 {
@@ -49,7 +31,7 @@ namespace SampleApp.WinUI3
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new Window();
+            m_window = new Window();            
          
             Frame rootFrame = new Frame();
             m_window.Content = rootFrame;
